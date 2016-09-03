100ef9c7b5499eb524034d089d989ac469a73f9f
Jamison Dance <jergason@gmail.com>
2016-09-03 11:32:13 -0600

    almost done with random numbers

--- topics/Effects/Random.elm
+++ topics/Effects/Random.elm
@@ -1,6 +1,6 @@
 module Effects.Random exposing (..)
 
-import Html exposing (div, h1, text, button)
+import Html exposing (div, h1, text, button, Html)
 import Html.App exposing (program)
 import Html.Events exposing (onClick)
 import Random exposing (generate)
@@ -32,10 +32,9 @@ import DetailedRendering.InlineStyles exposing (center)
 
    Well, what if we could create some kind of data structure that represented the thing we wanted Elm to do, and then hand that to Elm to run?
 -}
-
-
+{--
 type Msg
-    = RandomNumber Int
+    = NewNumber Int
 
 
 type alias AskElmForARandomNumber =
@@ -43,19 +42,15 @@ type alias AskElmForARandomNumber =
     , max : Int
     , result : Int -> Msg
     }
-
-
-
+--}
 {-
    Now, when we want to generate a random number, we can create this data structure that tells Elm what kind of thing we want. We can't do anything that causes side effects in our code, but what if we could somehow hand this data structure over to Elm?
 
    Then Elm could run the code that does side effects, grab the random number, put it in the result `Msg`, and then feed that `Msg` back in to our update function.
 -}
-
-
 {--
 aNumber =
-    { min = 0, max = 100, result = RandomNumber }
+    { min = 0, max = 100, result = NewNumber }
 
 
 type alias Model =
@@ -66,112 +61,117 @@ type alias Model =
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        RandomNumber n ->
+        NewNumber n ->
             { model | number = n }
 
 --}
-
-
 {-
-   Somehow `aNumber` could be processed by Elm to become a `RandomNumber` message, and fed back in to our update function.
+   Somehow `aNumber` could be processed by Elm to become a `NewNumber` message, and fed back in to our update function.
 
    Our `update` function is already a place where we hand data off to Elm for it to re-render our app. What if we changed `update` so instead of returning a model, it returns a model and the data structure for elm to run?
 
    Now the random number generation happens in two parts. One `Msg` hands Elm our data structure, then Elm runs it and feeds the resulting `Msg` back in to `update`, which results in our model being updated.
 -}
-
-
 {--
 type Msg
-    = RandomNumber Int
+    = NewNumber Int
     | Roll
 
 
 update : Msg -> Model -> ( Model, AskElmForARandomNumber )
 update msg model =
     case msg of
-        RandomNumber n ->
+        NewNumber n ->
             { model | number = n }
         Roll -> ( model, aNumber )
 
 --}
-
-
 {-
-We still have a problem though. Our update function won't compile, because not every `Msg` needs to hand Elm something to run for us. Now our case statement returns different types in different branches, which makes Elm sad. 
+   We still have a problem though. Our update function won't compile, because not every `Msg` needs to hand Elm something to run for us. Now our case statement returns different types in different branches, which makes Elm sad.
 
-We can solve that by wrapping the data structure in a Maybe. Then Elm can look in the Maybe, and if it has a Just, follow the loop that we imagined it doing.
+   We can solve that by wrapping the data structure in a Maybe. Then Elm can look in the Maybe, and if it has a Just, follow the loop that we imagined it doing.
 -}
-
-{--}
+{--
 type Msg
-    = RandomNumber Int
+    = NewNumber Int
     | Roll
 
 update : Msg -> Model -> ( Model, Maybe AskElmForARandomNumber )
 update msg model =
     case msg of
-        RandomNumber n ->
+        NewNumber n ->
             ({ model | number = n }, Nothing)
         Roll -> ( model, Just aNumber )
 
 
 --}
-
 {-
-There are still two missing pieces here.
+   There are still two missing pieces here.
 
-1. How do we tell Elm to handle our fancy data structure that represents some computation we want it to do?
-2. How do we handle other cases? This is very hard-coded to our specific use case, but what if you want to do Http requests or WebSockets or handle a clock ticking or any other kind of thing that depends on side-effects?
+   1. How do we tell Elm to handle our fancy data structure that represents some computation we want it to do?
+   2. How do we handle other cases? This is very hard-coded to our specific use case, but what if you want to do Http requests or WebSockets or handle a clock ticking or any other kind of thing that depends on side-effects?
 
+   The good news is that Elm has answers to both of these problems for us, and it looks a lot like what we've done so far. Let me show you some code, and I'll talk through it as I go.
 -}
 
--- The basic pattern is, you structure your code as a web of pure functions that are easy to test and re-use, and you push all the side-effects to the edges of your program, where Elm can handle them for you.
+-- TODO: this is a big jump
+-- TODO: explain the Cmd type
+{--}
 
 
-{--
-   model =
-       ( { number = 0 }, Cmd.none )
+type alias Model =
+    { number : Int
+    }
 
 
-   type Msg
-       = Roll
-       | NewNumber Int
+init : ( Model, Cmd Msg )
+init =
+    ( { number = 0 }, Cmd.none )
 
 
-   view model =
-       div [ center ]
-           [ h1 [] [ text "Ye Olde Random Number Generator" ]
-           , div [] [ button [ onClick Roll ] [ text "Make It So" ] ]
-           , div [] [ text ("The number is " ++ (toString model.number)) ]
-           ]
+type Msg
+    = Roll
+    | NewNumber Int
 
 
-   update msg model =
-       case msg of
-           Roll ->
-               ( model, getRandomNumbers )
-
-           NewNumber n ->
-               ( { model | number = n }, Cmd.none )
-
-
-   getRandomNumbers : Cmd Msg
-   getRandomNumbers =
-       Random.generate NewNumber (Random.int Random.minInt Random.maxInt)
+view : Model -> Html Msg
+view model =
+    div [ center ]
+        [ h1 [] [ text "Ye Olde Random Number Generator" ]
+        , div [] [ button [ onClick Roll ] [ text "Make It So" ] ]
+        , div [] [ text ("The number is " ++ (toString model.number)) ]
+        ]
 
 
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Roll ->
+            ( model, getRandomNumber )
 
-   {- ignore this part for now -}
+        NewNumber n ->
+            ( { model | number = n }, Cmd.none )
 
 
-   subscriptions model =
-       Sub.none
+getRandomNumber : Cmd Msg
+getRandomNumber =
+    Random.generate NewNumber (Random.int Random.minInt Random.maxInt)
 
 
-   main =
-       program { init = model, view = view, update = update, subscriptions = subscriptions }
 
+-- Don't worry about subscriptions now, we will cover them later
+
+
+subscriptions model =
+    Sub.none
+
+
+main =
+    program { init = init, view = view, update = update, subscriptions = subscriptions }
 --}
 
+
+
 -- EXERCISE: Modify this example to return two random numbers instead of just one
+
+-- The basic pattern is, you build your app out of pure functions that are easy to test and re-use, and you push all the side-effects to the edges of your program, where Elm can handle them for you.
