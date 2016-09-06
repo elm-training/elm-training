f9b4dd4f21b5caf4816ddf6709389e292c8c0d12
Sean Hess <seanhess@gmail.com>
2016-09-06 14:14:07 +0300

    Made Immutability assignment better

--- topics/Functional/Immutability.elm
+++ topics/Functional/Immutability.elm
@@ -76,19 +76,43 @@ scoreDeaths deaths =
 {- LEARN: Immutability - record updates -}
 
 
-gotKills : Int -> PlayerStats -> PlayerStats
-gotKills n stats =
-    -- live code: increment kills by n
-    { stats | kills = stats.kills + n }
+gotKill : PlayerStats -> PlayerStats
+gotKill stats =
+    -- live code: increment kills
+    { stats | kills = stats.kills + 1 }
+
+died : PlayerStats -> PlayerStats
+died stats =
+    -- live code: increment deaths
+    { stats | deaths = stats.deaths + 1 }
+
+
+collectedGold : Int -> PlayerStats -> PlayerStats
+collectedGold n stats =
+    -- live code, increment gold
+    { stats | gold = stats.gold + n }
 
 
 
 {- EXERCISE
-   If a player collects over 100 gold, they won the game. Add 1000 to their score.
+   1. During the course of a game, player "Tron" has the following happen, in order:
 
-   TODO: expand this so they actually have to add 1000 to the score of a player
+        - kill
+        - gold 200
+        - death
+        - kill
+        - kill
+        - gold 200
+        - death
+        - gold 800
 
-   Create a GameResult type alias that contains both the score and a boolean specifying whether they won. Write a function `gameResult` that returns a GameResult given PlayerStats.
+   Write a function that returns the final value of Tron's PlayerState after all of the above. Use the `gotKill`, `died`, and `collectedGold` functions, in order.
+
+   2. The game ends when one player collects 1000 gold, and that player wins. Add 1000 to their score.
+
+   Create a GameResult type alias that contains both the score and a boolean specifying whether a player won. Write a function `gameResult` that returns the GameResult given PlayerStats.
+
+   Print out the GameResult for Tron.
 
 -}
 
@@ -100,7 +124,6 @@ type alias GameResult =
 result : PlayerStats -> GameResult
 result stats =
     Debug.crash "TODO"
-
 
 
 main =
